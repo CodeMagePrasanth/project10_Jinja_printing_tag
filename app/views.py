0e734af67d820101ad7aaa7ceb0f75a2702accfa
prasanth <prasanthsenthilkumaran@gmail.com>
2023-07-27 12:08:40 +0530

    firt comm

--- app/views.py
+++ app/views.py
@@ -2,5 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def data_render(request):
-    d={'anime':'Naruto'}
+
+    d={'anime':'Naruto','episodes':700}
+
     return render(request,'data_render.html',context=d)
